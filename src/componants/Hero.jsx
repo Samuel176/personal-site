8aba74b63bd8fbe08781d544c6f7d3b74bdbe0b1
sam elliott <73132951+Samuel176@users.noreply.github.com>
2025-11-12 14:01:15 +0000

    Add files via upload

---
+++ src/componants/Hero.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Hero() {
+    return <div className="hero">
+            <div className="hero__container">
+                <h1 className="heading-primary margin-bottom-m">Sam's Portfolio</h1>
+                <h2 className="heading-secondary margin-bottom-m">Web Developer Extraordinaire</h2>
+                <a href="" className="btn">View All Projects</a>
+                <div className="hero__container-info">
+                    <h2 className="heading-secondary margin-bottom-m">Development | Design</h2>
+                  
+                    
+
+                </div>
+            </div>
+
+    </div>
+}
+
+export default Hero;
