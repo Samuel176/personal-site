a6eeaf2a28353da74274fece5dcbadb641026250
sam elliott <73132951+Samuel176@users.noreply.github.com>
2025-11-12 16:35:05 +0000

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Hero from './componants/Hero'
+import Projects from './componants/Projects'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +11,7 @@ function App() {
   return (
     <div>
       <Hero />
- 
+      <Projects />
     </div>
   )
 }
--- src/componants/Projects.jsx
+++ src/componants/Projects.jsx
@@ -2,8 +2,20 @@ import React from "react";
 
 function Projects(){
     return <div className="projects">
-
-    </div>
+                <div className="projects__container">
+                    <img src="public/img/naughton.png" alt="" className="projects__container-img" />
+                    <p className="projects__container-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil harum autem voluptatem.</p>
+                </div>
+                <div className="projects__container">
+                    <img src="public/img/naughton.png" alt="" className="projects__container-img" />
+                    <p className="projects__container-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil harum autem voluptatem.</p>
+                </div>
+                <div className="projects__container">
+                    <img src="public/img/naughton.png" alt="" className="projects__container-img" />
+                    <p className="projects__container-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil harum autem voluptatem.</p>
+                </div>
+          
+        </div>
 }
 
 export default Projects;
