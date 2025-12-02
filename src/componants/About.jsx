bf8b2a645a202e0173af8371b7d827c72675f145
sam elliott <73132951+Samuel176@users.noreply.github.com>
2025-12-02 15:54:24 +0000

    Add files via upload

--- src/componants/About.jsx
+++ src/componants/About.jsx
@@ -3,10 +3,20 @@ import React from "react";
 function About(props){
     return <div className="about-section">
                 <div className="about-content">
-                    <h1 className="heading-primary margin-l">About Me</h1>
+                    <h1 className="heading-primary padding-m">About Me</h1>
                     <div className="about-content__container">
-                        <div className="about-content__container-box"></div>
-                        <div className="about-content__container-box"></div>
+                        <div className="about-content__container-box">
+                            <div className="image-text-container">
+                                <img className="image-text-container__image" src="img/meOnTrain.jpeg" alt="" />
+                                <div className="image-text-container__text">
+                                    <h3 className="heading-tertiary">Pillar Of the Community</h3>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, ut rem. Rerum eligendi</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="about-content__container-box">
+                            
+                        </div>
                     </div>
                 </div>
                 <div className="bg-video">
