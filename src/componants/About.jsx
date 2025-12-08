4ecf83f73ba8cbeb882193f77f5b81bcb51aec64
sam elliott <73132951+Samuel176@users.noreply.github.com>
2025-12-08 14:33:29 +0000

    Add files via upload

--- src/componants/About.jsx
+++ src/componants/About.jsx
@@ -4,12 +4,18 @@ import imageText from "../assets/imageText"
 
 
 function About(props){
+
+    const left = imageText.slice(0,2)
+    const right = imageText.slice(2,imageText.length)
+
+
+
     return <div className="about-section">
                 <div className="about-content">
                     <h1 className="heading-primary padding-m">About Me</h1>
                     <div className="about-content__container">
                         <div className="about-content__container-box">
-                            {imageText.map((p) => (
+                            {left.map((p) => (
                                 <ImageBox 
                                     key= {p.id}
                                     title= {p.title}
@@ -19,7 +25,14 @@ function About(props){
                             ))}
                         </div>
                         <div className="about-content__container-box-right">
-                            
+                            {right.map((p) => (
+                                <ImageBox 
+                                    key= {p.id}
+                                    title= {p.title}
+                                    text= {p.text}
+                                    img= {p.img}
+                                />
+                            ))}
                         </div>
                     </div>
                 </div>
