11712f46d3030acc1a44d884f86d1ddce6d4d9ef
sam elliott <73132951+Samuel176@users.noreply.github.com>
2025-12-17 08:05:48 +0000

    Add files via upload

--- src/componants/Contact.jsx
+++ src/componants/Contact.jsx
@@ -6,6 +6,8 @@ function contact(props){
         <div className="contact__info">
             <p className="contact__info-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos error non,
                  eveniet voluptas dicta quis eligendi voluptate nostrum ipsam fugit esse sed voluptatum voluptatem sequi nisi iste. Suscipit, beatae eaque!</p>
+            <p className="contact__info-text" ></p>
+            <p className="contact__info-text" ></p>
         </div>
     </div>
 
