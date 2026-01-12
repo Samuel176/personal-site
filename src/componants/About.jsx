526c156c8236039756d2791c4e9ebe276e19949e
sam elliott <73132951+Samuel176@users.noreply.github.com>
2026-01-12 14:24:54 +0000

    Add files via upload

--- src/componants/About.jsx
+++ src/componants/About.jsx
@@ -5,16 +5,17 @@ import imageText from "../assets/imageText"
 
 function About(props){
     return <div className="about-section">
-        
-        {/* {imageText.map((p) => (
+        <h2 className="heading-secondary">About</h2>
+        <div className="about-section__container">
+            {imageText.map((p) => (
             <ImageBox 
                 key={p.id}
                 img={p.img}
                 text={p.text}
                 title={p.title}
             />
-        ))} */}
-        
+        ))}
+        </div>
     </div>
 }
 
--- src/componants/ImageBox.jsx
+++ src/componants/ImageBox.jsx
@@ -2,15 +2,18 @@ import React from "react";
 
 function ImageBox(props){
     return ( 
-    <div className="image-text-container">
-        <div className="image-text-container__image">
-            <div className="image-text-container__image">
-                <img className="" src={props.img} alt="" />
+    <div className="image-box">
+        <div className="image-text-container">
+          
+                <img className="image-text-container__image" src={props.img} alt="" />
+            
+            <div className="image-text-container__text">
+                <h3 className="heading-tertiary">{props.title}</h3>
+                <p>{props.text}</p>
             </div>
         </div>
-        <div className="image-text-container__text">
-            <h3 className="heading-tertiary">{props.title}</h3>
-            <p>{props.text}</p>
+        <div className="image-box__back">
+
         </div>
     </div>
     )
