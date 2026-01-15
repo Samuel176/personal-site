cf871c07b4bf86d19669fa5b37f57fa6e4ee0763
sam elliott <73132951+Samuel176@users.noreply.github.com>
2026-01-15 12:10:02 +0000

    Add files via upload

--- src/componants/ImageBox.jsx
+++ src/componants/ImageBox.jsx
@@ -1,19 +1,36 @@
 import React from "react";
+import { useState } from 'react';
+
+
 
 function ImageBox(props){
+    const [isFlipped, setIsFlipped] = React.useState(false)
     return ( 
-    <div className="image-box">
-        <div className="image-text-container">
-            <div className="image-text-container__image">
-                <img className="image-text-container__image-image" src={props.img} alt="" />
-            </div>
-            <div className="image-text-container__text">
-                <h3 className="heading-tertiary image-text-container__text-heading">{props.title}</h3>
-                <p>{props.text}</p>
+    <div className={`image-box ${isFlipped ? "image-box--flipped" : ""} `}
+        onClick={() => setIsFlipped(f => !f)}
+    >
+        <div className="image-box__front">
+            <div className="image-text-container">
+                <div className="image-text-container__image">
+                    <img className="image-text-container__image-image" src={props.img} alt="" />
+                </div>
+                <div className="image-text-container__text">
+                    <h3 className="heading-tertiary image-text-container__text-heading">{props.title}</h3>
+                    <p>{props.text}</p>
+                </div>
             </div>
         </div>
+        
         <div className="image-box__back">
-
+            <div className="image-text-container">
+                <div className="image-text-container__image">
+                    <img className="image-text-container__image-image" src={props.imgBack} alt="" />
+                </div>
+                <div className="image-text-container__text">
+                    <h3 className="heading-tertiary image-text-container__text-heading">{props.titleBack}</h3>
+                    <p>{props.textBack}</p>
+                </div>
+            </div>
         </div>
     </div>
     )
